0363bc63e0857ffd2276f4b331bb824230a01c8c
marcusljx <marcusljx@gmail.com>
2018-03-19 01:37:56 +0800

    finishing preliminary edit

--- marcusljx-setup.sh
+++ marcusljx-setup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash 
 
 RC_FILE="~/.zshrc"
+REBOOT=false
 
 function say() {
     echo "=========== $@"
 }
 
+#======================================
 say "[INSTALL] vim"
 sudo apt install -y vim
 
+#======================================
 say "[INSTALL] zsh + oh-my-zsh "
 sudo apt install -y vim
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
 
+#======================================
+say "[SETTING] Set 3-finger tap to middle-click"
+synclient TapButton3=2
+
+#======================================
+say "[APPEND] adding source cmds to ${RC_FILE}"
 echo '## MARCUSLJX SCRIPTS' >> ${RC_FILE}
-echo 'source ${SCRIPTS}/env/*' >> ${RC_FILE}
+echo 'source ${HOME}/scripts/.src' >> ${RC_FILE}
