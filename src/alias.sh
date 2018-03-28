302ccabe39a03815eabd0804687b6c58d908688a
marcusljx <marcusljx@gmail.com>
2018-03-29 07:15:54 +0800

    adding kubectl alias

--- src/alias.sh
+++ src/alias.sh
@@ -6,9 +6,15 @@
 JETBRAINS_APPS_HOME=${HOME}/.local/share/JetBrains/Toolbox/apps
 function jb_run() {
     # $1 <- App Dir Name (Goland, PyCharm, etc.)
-    lowercaseApp=$(echo $1 | tr '[:upper:]' '[:lower:]')
+    # $2 <- bin script (goland.sh, pycharm.sh, etc.)
     latestVersion=$(ls ${JETBRAINS_APPS_HOME}/$1/ch-0 | head -n 1)
 
-    ${JETBRAINS_APPS_HOME}/$1/ch-0/${latestVersion}/bin/${lowercaseApp}.sh
+    ${JETBRAINS_APPS_HOME}/$1/ch-0/${latestVersion}/bin/$2.sh
 }
-alias goland="jb_run Goland"
+alias goland="jb_run Goland goland"
+alias pycharm="jb_run PyCharm-P pycharm"
+
+###########################
+# SHORT FORMS 
+###########################
+alias k=kubectl
