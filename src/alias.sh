2ad7d50d53cdafb0d397e64b58366a168f61ef96
marcusljx <marcusljx@gmail.com>
2018-04-10 09:57:56 +0800

    adding changes to JB detection

--- src/alias.sh
+++ src/alias.sh
@@ -7,9 +7,7 @@ JETBRAINS_APPS_HOME=${HOME}/.local/share/JetBrains/Toolbox/apps
 function jb_run() {
     # $1 <- App Dir Name (Goland, PyCharm, etc.)
     # $2 <- bin script (goland.sh, pycharm.sh, etc.)
-    latestVersion=$(ls ${JETBRAINS_APPS_HOME}/$1/ch-0 | head -n 1)
-
-    ${JETBRAINS_APPS_HOME}/$1/ch-0/${latestVersion}/bin/$2.sh
+    $(ls -d ${JETBRAINS_APPS_HOME}/$1/ch-0/*/ | head -n 1)/bin/$2.sh
 }
 alias goland="jb_run Goland goland"
 alias pycharm="jb_run PyCharm-P pycharm"
@@ -18,3 +16,4 @@ alias pycharm="jb_run PyCharm-P pycharm"
 # SHORT FORMS 
 ###########################
 alias k=kubectl
+alias mk=minikube
