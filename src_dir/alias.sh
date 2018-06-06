c401bc0e26a3b7a135e008fb4baab18a8c1538ba
marcusljx <marcusljx@gmail.com>
2018-06-07 03:11:44 +0800

    update

--- src_dir/alias.sh
+++ src_dir/alias.sh
@@ -3,14 +3,14 @@
 ###########################
 # JETBRAINS TOOLBOX PATHS 
 ###########################
-JETBRAINS_APPS_HOME=${HOME}/.local/share/JetBrains/Toolbox/apps
-function jb_run() {
-    # $1 <- App Dir Name (Goland, PyCharm, etc.)
-    # $2 <- bin script (goland.sh, pycharm.sh, etc.)
-    $(ls -d ${JETBRAINS_APPS_HOME}/$1/ch-0/*/ | head -n 1)/bin/$2.sh
-}
-alias goland="jb_run Goland goland"
-alias pycharm="jb_run PyCharm-P pycharm"
+#JETBRAINS_APPS_HOME=${HOME}/.local/share/JetBrains/Toolbox/apps
+#function jb_run() {
+#    # $1 <- App Dir Name (Goland, PyCharm, etc.)
+#    # $2 <- bin script (goland.sh, pycharm.sh, etc.)
+#    $(ls -d ${JETBRAINS_APPS_HOME}/$1/ch-0/*/ | head -n 1)/bin/$2.sh
+#}
+#alias goland="jb_run Goland goland"
+#alias pycharm="jb_run PyCharm-P pycharm"
 
 ###########################
 # SHORT FORMS 
